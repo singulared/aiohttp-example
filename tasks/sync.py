952c215118508598d3956f74128b2f870ab1221d
Belousow Makc <lib.bmw@gmail.com>
2015-09-23 23:34:51 +0300

    Remove debug sleep from sync.counter_list

--- tasks/sync.py
+++ tasks/sync.py
@@ -8,7 +8,6 @@ from yandex import get_counters
 def counter_list(loop, app):
     try:
         app.logger.debug('sync.counter_list - started')
-        yield from asyncio.sleep(10)
         counters = yield from get_counters()
         yield from app.redis.set('counters', ujson.dumps(counters))
         app.logger.debug('sync.counter_list - ended')
